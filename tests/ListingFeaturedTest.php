42eb4966a699147c49f7a59fd0c8c33e78e498aa
Evija Zante <evija.zante@gmail.com>
2021-01-25 18:22:01 +0200

    Task 11: Test ListingFeatured CoC can be set and retrieved

--- tests/ListingFeaturedTest.php
+++ tests/ListingFeaturedTest.php
@@ -17,4 +17,16 @@ class ListingFeaturedTest extends TestCase
 
         $this->assertEquals('featured', (new ListingFeatured($data))->getStatus());
     }
+
+    /** @test */
+    public function cocCanBeSetAndRetrieved()
+    {
+        $data = [
+            'id' => 1,
+            'title' => 'Title',
+            'coc' => 'Code of Conduct',
+        ];
+
+        $this->assertEquals($data['coc'], (new ListingFeatured($data))->getCoc());
+    }
 }
